3fb4c2012fb4cbc6b01f10fe521b6a474019356a
alixchan <kubi1-ya@ya.ru>
2025-04-04 15:34:24 +0300

    add problems

---
+++ src/BrowserHistory1472.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+
+public class BrowserHistory1472 {
+
+    private LinkedList<String> history = new LinkedList<>();
+    private int pointer = 0;
+
+    public BrowserHistory1472(String homepage) {
+        history.add(homepage);
+    }
+
+    public void visit(String url) {
+        while (history.size() > pointer + 1) {
+            history.removeLast();
+        }
+        history.add(url);
+        pointer++;
+    }
+
+    public String back(int steps) {
+        pointer -= Math.min(steps, pointer);
+        return history.get(pointer);
+    }
+
+    public String forward(int steps) {
+        int actualSteps = Math.min(steps, history.size() - 1 - pointer);
+        pointer += actualSteps;
+        return history.get(pointer);
+    }
+
+}
+
+
+
